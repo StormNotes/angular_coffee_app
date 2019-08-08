f1c5123091363c7fb8fe5ad374d676e659f0c900
leonardo <leonardo.diaz@mrm-mccnann.com>
2019-08-08 16:11:56 -0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myngapp';
+  title = 'This is our NG App!';
+  age = 12;
+  muse = 'She';
 }
